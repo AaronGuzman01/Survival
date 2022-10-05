9e376e71072dba50ba3e28496ab71b5f32055872
Aaron <aaronguzman209@gmail.com>
2022-10-05 02:14:56 -0400

    Added orb generation, updated orb types, added an attract field, and started terrain generation testing.

---
+++ Survival/Assets/Scripts/ItemAttracter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemAttracter : MonoBehaviour
+{
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ItemData.scale = transform.localScale.x;
+        transform.position = player.position;
+
+        foreach (Collider2D col in Physics2D.OverlapBoxAll(transform.position, transform.localScale, 0f))
+        {
+            if (col.GetComponent<ItemData>())
+            {
+                col.GetComponent<ItemData>().SetFollow();
+            }
+        }
+    }
+}
--- Survival/Assets/Scripts/PlayerData.cs
+++ Survival/Assets/Scripts/PlayerData.cs
@@ -5,11 +5,13 @@ public class PlayerData : MonoBehaviour
     [SerializeField]
     private float health;
     private float experience;
+    private int level;
 
     private void Start()
     {
         health = 100;
         experience = 0;
+        level = 0;
     }
 
     public float GetHealth()
@@ -17,8 +19,26 @@ public class PlayerData : MonoBehaviour
         return health;
     }
 
+    public float GetExperience()
+    {
+        return experience;
+    }
+
     public void UpdateHealth(float health)
     {
         this.health = health;
+    }
+
+    public void UpdateExperience(float experience)
+    {
+        if (this.experience + experience > 100)
+        {
+            this.experience = 0;
+            level++;
+        }
+        else
+        {
+            this.experience += experience;   
+        }
     }
 }
